75378ccbd9897ae8b2e24255e2284daffe08c353
gbrParanhos <gbrparanhos@gmail.com>
2025-02-01 20:08:04 -0300

    ajuste no template/Auth

--- src/components/templates/Auth/index.tsx
+++ src/components/templates/Auth/index.tsx
@@ -1,7 +1,7 @@
 import { ITemplateProps } from '@/types';
 
 const AuthTemplate = ({ children }: ITemplateProps) => (
-	<div className='h-screen max-h-screen bg-background-light overflow-scroll lg:overflow-hidden py-8'>
+	<div className='h-screen bg-background-light overflow-scroll lg:overflow-hidden py-8'>
 		{children}
 	</div>
 );
